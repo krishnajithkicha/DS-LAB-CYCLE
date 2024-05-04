9817d054592bc72e00dbb27f9ef3fad79dc6dab8
krishnajithkicha <krishnajithkicha@gmail.com>
2024-05-04 14:40:24 +0530

    DS

--- Binary Tree/Binary tree using linkedlist/BinaryTree.h
+++ Binary Tree/Binary tree using linkedlist/BinaryTree.h
@@ -1,13 +1,26 @@
 #include<iostream>
 using namespace std;
-class Node{
-	int data;
-	Node* llink;
-	Node* rlink;
-	public:
-		Node(T);
-		
-		void set_llink(Node*);
-		void set_rlink(Node*);
-		
+class Node {
+public:
+    int data;
+    Node* left;
+    Node* right;
+    Node(int);
+    int get_data();
+};
+
+class BinaryTree {
+private:
+    Node* root;
+    void insert_node(Node*&, int);
+    void in_order(Node*);
+    void pre_order(Node*);
+    void post_order(Node*);
+
+public:
+    BinaryTree();
+    void insert(int);
+    void inorder();
+    void preorder();
+    void postorder();
 };
